ef99c856b9e067e9294f0b1500738ea33d827677
Carol Robles B <carolrobleseduc@gmail.com>
2024-03-01 13:03:13 -0300

    ADD: agregando alerta de texto campo obligatorio

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const inputEmail = document.querySelector("#email");
     const inputAsunto = document.querySelector("#asunto");
     const inputMensaje = document.querySelector("#mensaje");
+    const formulario = document.querySelector("#formulario");
 
     //Asignar eventos 
     inputEmail.addEventListener("blur", validar);
@@ -11,6 +12,20 @@ document.addEventListener("DOMContentLoaded", function () {
     inputMensaje.addEventListener("blur", validar);
     
     function validar(e) {
-        console.log(e.target.value);
+        if (e.target.value.trim() === "") {
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+        } else{
+            console.log("si hay algo...")
+        }
+    }
+
+    function mostrarAlerta(mensaje, referencia) {
+        const error = document.createElement("P");
+        error.textContent = mensaje;
+        error.classList.add("bg-red-600", "text-white", "p-2");
+   
+
+        //inyectar el error al formulario
+        referencia.appendChild(error)
     }
 });
